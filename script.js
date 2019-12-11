48391ab648a258c185fbd836ed28233af6076581
Ryan Larsen <larsenr12@gmail.com>
2019-12-11 13:36:11 -0500

    Added Input/Save buttons/Hour to each div

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ $(document).ready(function () {
 
   $('#date').html(now)
 
-
+function inputCreate () {
+  
+}
 
 
 
@@ -12,3 +14,15 @@ $(document).ready(function () {
 
 // Need to use moments to set hour of the object they pass into the input forms
 
+//Day Planner homework - we are asked to display standard business hours and each hour should contain: The time
+//A field to hold user input
+//A save button and each timeblock is color coded to indicate whether it is in a past, present, or future hour. Should this be coded using html and css or dynamically added to the page using javascript?
+
+
+//Day Planner homework - we are asked to display standard business hours and each hour should contain:
+//1. The time
+//2. A field to hold user input
+//3. A save button
+//Each time-block is color coded to indicate whether it is in a past, present, or future hour. Should this be coded using HTML and CSS or dynamically added to the page using JavaScript?
+
+//Use a form to allow enter button for input
